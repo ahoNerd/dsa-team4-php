fa54afa5e34aceae38bb33b5186b9c030942f9b9
ahoNerd <ramdan@ahonerd.com>
2023-07-14 16:16:47 +0700

    update main screen

--- Includes/head.php
+++ Includes/head.php
@@ -2,6 +2,9 @@
 include("var.php");
 ?>
 
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+
 <title><?=$app_name?></title>
 
 <link rel="stylesheet" type="text/css" href="Styles/AgroCraft.css">	
--- index.php
+++ index.php
@@ -11,11 +11,21 @@ include("Includes/head.php");
 </head>
 
 <body >
+	<div id="con1">
+		<div id="con2">
+			<div id="appName"><?=$app_name?></div>
 	
-	<div id="WELCOME"><?=$app_name?></h1>
-	<span id="linebreak"></span>
-	<label id="Farmerbutton"><a id = "link" href="auth/FarmerLogin.php">FARMER</a></label>
-	<label id="Buyerbutton"><a id = "link" href="auth/BuyerLogin.php">BUYER</a></label>
+			<div id="con3">
+				<a class="home-btn" href="auth/FarmerLogin.php">FARMER</a>
+				<a class="home-btn" href="auth/BuyerLogin.php">BUYER</a>
+			</div>
+		</div>
+	</div>
+
+	<div id="about">
+		<strong>Kelompok 4</strong>
+		<div>Aplikasi ini dibuat untuk memenuhi tugas mata kuliah Desain dan Analisis Algoritma <span class="whitespace-nowrap">(Strategi Algoritma)</span></div>
+	</div>
 	
 </body>
 </html>
