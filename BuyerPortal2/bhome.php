9e1d2e6d5bf12dcaabcb7234e36ce907988313d2
ahoNerd <ramdan@ahonerd.com>
2023-07-15 02:56:36 +0700

    remove unnecessary link

--- BuyerPortal2/bhome.php
+++ BuyerPortal2/bhome.php
@@ -13,7 +13,6 @@ include("../Functions/functions.php");
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-    <a href="https://icons8.com/icon/83325/roman-soldier"></a>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <script src="https://kit.fontawesome.com/c587fc1763.js" crossorigin="anonymous"></script>
@@ -675,10 +674,13 @@ include("../Functions/functions.php");
                 </div>
                 </hr>
             </div>
+
+
             <div class="row">
                 <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center">
-                    <p><u><a href="https://www.agrocraft.com/">AgroCraft Corporation</a></u> is a Multitrading Company for farmers ang traders</p>
-                    <p class="h6">Copy All right Reversed.<a class="text-green ml-2" href="https://www.google.com" target="_blank">Agrotech</a></p>
+<?php
+include("../Includes/footer.php");
+?>	
                 </div>
                 </hr>
             </div>
--- Includes/footer.php
+++ Includes/footer.php
@@ -1,4 +1,4 @@
-<div id="theFooter">
+<div id="theFooter" class="text-center p-3">
   <strong>Kelompok 4</strong>
   <div>Aplikasi ini dibuat untuk memenuhi tugas mata kuliah Desain dan Analisis Algoritma <span class="whitespace-nowrap">(Strategi Algoritma)</span></div>
 </div>
