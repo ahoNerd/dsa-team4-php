dcc3c0278b5b992d5a0c8c66c94c297aa7fda8e9
ahoNerd <ramdan@ahonerd.com>
2023-07-14 15:36:08 +0700

    change mysql credential

--- Admin/includes/db.php
+++ Admin/includes/db.php
@@ -1,3 +1,3 @@
 <?php 
-        $con = mysqli_connect("localhost","root","","n1602954_impulse101");
+        $con = mysqli_connect("localhost","n1602954_dsa","dsa-team4","n1602954_impulse101");
        
--- Includes/db.php
+++ Includes/db.php
@@ -1,6 +1,6 @@
 <?php 
 
-        $con = mysqli_connect("localhost","root","","n1602954_impulse101");
+        $con = mysqli_connect("localhost","n1602954_dsa","dsa-team4","n1602954_impulse101");
       
         if (mysqli_connect_errno()) {
                 echo "Failed to connect to MySql " . mysqli_connect_error();
